4b0c9cc82db5685076d7c76b3f371c2b49bd5d0a
bmcilhenny <bmcilhen@gmail.com>
2019-07-06 20:58:32 -0400

    add notification badge to header, counts total number of bells currently active

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 var main = function() {
-    $('.more-btn').on('click', (event) => {
-      $(event.currentTarget).siblings('.more-menu').toggle();
-    });
-    $('.share').on('click', (event) => {
-      $(event.currentTarget).next().toggle();
-    });
-    
-    $('.notification').on('click', (event) => {
-      $(event.currentTarget).toggleClass('active');
-    })
-    
-    
-  };
+  let numberActive = $('.glyphicon-bell.active').length;
+  // wrong spot, it's not in the local scope;
+  // we'll add a watch to numberActive to see if it changes, if it does not then we know something is wrong
+  $('.more-btn').on('click', (event) => {
+    $('.more-menu').toggle();
+  });
+  $('.share').on('click', (event) => {
+    $(event.currentTarget).next().toggle();
+  });
+  
+  $('.notification').on('click', (event) => {
+    // first step
+    const target = event.target;
+    console.log(event);
+    // second step
+    console.log('You clicked on the item, ', target);
+    $(event.currentTarget).toggleClass('active');
+    // we must add this check to remove the content within the notification badge and thus the notification badge itself when there are no bells currently active
+    if (numberActive) {
+      $('#total-notifications').text(numberActive);
+
+    } else {
+      $('#total-notifications').text('');
+    }
+  })    
+};
   
   $(document).ready(main);
   
+// var main = function() {
+//   $('.more-btn').on('click', (event) => {
+//     $(event.currentTarget).siblings('.more-menu').toggle();
+//   });
+//   $('.share').on('click', (event) => {
+//     $(event.currentTarget).next().toggle();
+//   });
+  
+//   $('.notification').on('click', (event) => {
+//     // first step
+//     const target = event.target;
+//     console.log(event);
+//     // second step
+//     console.log('You clicked on the item, ', target);
+//     $(event.currentTarget).toggleClass('active');
+//   })
+  
+  
+// };
+
+// $(document).ready(main);
   
