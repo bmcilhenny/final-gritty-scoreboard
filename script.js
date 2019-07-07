27d6d772dd24fdd0b43e8b03faa88d1e00d89610
bmcilhenny <bmcilhen@gmail.com>
2019-07-06 22:54:21 -0400

    add console.logs, comments for easier video recording

--- script.js
+++ script.js
@@ -3,15 +3,20 @@ var main = function() {
   // wrong spot, it's being declared too early;
   // we'll add a watch to numberActive to see if it changes, if it does not then we know something is wrong
   $('.more-btn').on('click', (event) => {
+    debugger;
     let menu = $('.more-menu');
+    // first step
+    //  console.log(menu)
+    //  second step
+    //  console.log('How many items are being toggled?', menu.length);
     // adds breakpoint
-    $('.more-menu').toggle();
-    console.log("HERE")
+    menu.toggle();
   });
   
   $('.notification').on('click', (even) => {
     // first step
     $(event.currentTarget).toggleClass('active');
+    console.log("HERE")
     let numberActive = $('.glyphicon-bell.active').length;
     // we must add this check to remove the content within the notification badge and thus the notification badge itself when there are no bells currently active
     // if (numberActive) {
