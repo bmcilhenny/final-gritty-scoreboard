cb0ab7c04d74362c55e6203d7d1de538577b8136
bmcilhenny <bmcilhen@gmail.com>
2019-07-06 21:53:34 -0400

    remove lines from walkthrough

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 var main = function() {
-  let numberActive = $('.glyphicon-bell.active').length;
-  // wrong spot, it's not in the local scope;
+  // let numberActive = $('.glyphicon-bell.active').length;
+  // wrong spot, it's being declared too early;
   // we'll add a watch to numberActive to see if it changes, if it does not then we know something is wrong
   $('.more-btn').on('click', (event) => {
+    let menu = $('.more-menu');
+    // adds breakpoint
     $('.more-menu').toggle();
-  });
-  $('.share').on('click', (event) => {
-    $(event.currentTarget).next().toggle();
+    console.log("HERE")
   });
   
-  $('.notification').on('click', (event) => {
+  $('.notification').on('click', (even) => {
     // first step
-    const target = event.target;
-    console.log(event);
-    // second step
-    console.log('You clicked on the item, ', target);
     $(event.currentTarget).toggleClass('active');
+    let numberActive = $('.glyphicon-bell.active').length;
     // we must add this check to remove the content within the notification badge and thus the notification badge itself when there are no bells currently active
-    if (numberActive) {
-      $('#total-notifications').text(numberActive);
+    // if (numberActive) {
+    //   $('#total-notifications').text(numberActive);
 
-    } else {
-      $('#total-notifications').text('');
-    }
+    // } else {
+    //   $('#total-notifications').text('');
+    // }
   })    
 };
   
