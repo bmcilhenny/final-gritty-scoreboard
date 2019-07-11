f4dd7641f014f11caf7e62e363f67e1a98189fdf
bmcilhenny <bmcilhen@gmail.com>
2019-07-11 18:49:53 -0400

    convert jquery to vanil js, add note about nodelist vs html collection

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-var main = function() {
+const main = function() {
+  // using querySelectorAll cause they're more modern, they return a Nodelist as opposed to a HTML collection like getElementsByClassName. You cannot call .forEach on a HTML collection BUT can on a Node List. Ugh... DOM unintuitiveness
+  const menues = document.querySelectorAll('.more-menu');
   // let numberActive = $('.glyphicon-bell.active').length;
   // wrong spot, it's being declared too early;
   // we'll add a watch to numberActive to see if it changes, if it does not then we know something is wrong
-  $('.more-btn').on('click', (event) => {
-    debugger;
-    let menu = $('.more-menu');
-    // first step
-    //  console.log(menu)
-    //  second step
-    //  console.log('How many items are being toggled?', menu.length);
-    // adds breakpoint
-    menu.toggle();
-  });
-  
-  $('.notification').on('click', (even) => {
-    // first step
-    $(event.currentTarget).toggleClass('active');
-    console.log("HERE")
-    let numberActive = $('.glyphicon-bell.active').length;
-    // we must add this check to remove the content within the notification badge and thus the notification badge itself when there are no bells currently active
-    // if (numberActive) {
-    //   $('#total-notifications').text(numberActive);
+  document.addEventListener('click', (event) => {
+       if (event.target.matches('.more-btn')) {
+         debugger;
+         menues.forEach((menu) => {
+           if ( getComputedStyle(menu).display === 'none') {
+             debugger;
+             menu.style.setProperty('display', 'block');
+           } else {
+            menu.style.setProperty('display', 'none');
+           }
+         })
+       }
 
-    // } else {
-    //   $('#total-notifications').text('');
-    // }
-  })    
+       if (event.target.matches('.notification')) {
+        event.target.classList.contains('active') ? event.target.classList.remove('active') : event.target.classList.add('active');
+        console.log("HERE");
+        let numberActive = document.querySelectorAll('.glyphicon-bell.active').length;
+        // numNumberActive = numberActive.empty? ;
+        // we must add this check to remove the content within the notification badge and thus the notification badge itself when there are no bells currently active
+        // if (numberActive) {
+        //   $('#total-notifications').text(numberActive);
+
+        // } else {
+        //   $('#total-notifications').text('');
+        // }
+       }
+          //  debugger;
+          //  first step
+          //   console.log(menu)
+          //   second step
+          //   console.log('How many items are being toggled?', menu.length);
+          //  adds breakpoint
+          // debugger;
+  });
+
 };
   
-  $(document).ready(main);
+
+document.addEventListener("DOMContentLoaded", main);
   
